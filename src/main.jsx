3416da7bcc54cf07e2a0ab8863b64dee2f9988f6
Dodge <93716715+AnastasiiaKrtsk@users.noreply.github.com>
2024-01-09 11:38:32 +0200

    project-init-json

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 // import store from "redux/store";
 
 import "./index.css";
