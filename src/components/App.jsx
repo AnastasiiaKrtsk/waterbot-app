aafde3e3bedd32437f7c7ac9b6d6dee80deca0fe
Dodge <93716715+AnastasiiaKrtsk@users.noreply.github.com>
2024-01-09 16:25:55 +0200

    layout-app-css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,22 @@
+// import { Route, Routes } from "react-router-dom";
+import WelcomePage from "../pages/WelcomePage/WelcomePage";
+import Layout from "./Layout/Layout";
+// import PublicRoute from "./PublicRoute/PublicRoute";
+
 const App = () => {
-  return <div>Appppppp</div>;
+  return (
+    <>
+      {/* <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="/welcome" element={<WelcomePage />} />
+        <Route path="" element={<PublicRoute />}>
+          <Route path="/auth" element={<Auth />} />
+        </Route>
+      </Routes> */}
+      <Layout />
+      <WelcomePage />
+    </>
+  );
 };
 
 export default App;
--- src/redux/slice.js
+++
@@ -1,3 +0,0 @@
-export const slice = () => {
-  return <div>slice</div>;
-};
