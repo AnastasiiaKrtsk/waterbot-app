18c505b1170e578b0ff5afee2e98a947c8421987
Dodge <93716715+AnastasiiaKrtsk@users.noreply.github.com>
2024-01-09 10:15:50 +0200

    project-init-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import ReactDOM from "react-dom/client";
-
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import Loader from "components/Loader";
+import { App } from "./components/App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<></>);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <Provider store={store}>
+      <PersistGate loading={<Loader />} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </>
+);
