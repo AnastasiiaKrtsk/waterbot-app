ff91583872bed925c3eb0566a77f247be08e0f73
Dodge <93716715+AnastasiiaKrtsk@users.noreply.github.com>
2024-01-09 11:23:46 +0200

    project-init-final-fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+// import store from "redux/store";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    {/* <Provider> */}
+    <App />
+    {/* </Provider> */}
   </React.StrictMode>
 );
