e2bad5b3786179ab959f8f15fdd0634a375dc613
Aleksandr2905 <natalukha@ukr.net>
2024-01-10 15:12:04 +0200

    added store & slice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import WelcomePage from "../pages/WelcomePage/WelcomePage";
 import Layout from "./Layout/Layout";
+import PublicRoute from "./PublicRoute/PublicRoute";
+import SignInPage from "../pages/SignInPage/SignInPage";
+import SignUpPage from "../pages/SignUpPage/SignUpPage";
 
 const App = () => {
   return (
@@ -9,6 +12,22 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<WelcomePage />} />
         </Route>
+        <Route
+          path="signup"
+          element={
+            <PublicRoute>
+              <SignUpPage />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/signin"
+          element={
+            <PublicRoute>
+              <SignInPage />
+            </PublicRoute>
+          }
+        />
       </Routes>
     </>
   );
