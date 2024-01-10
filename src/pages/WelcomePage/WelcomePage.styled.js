2dabc7ce06fa9bd6edf8826ef34c958de3842cb2
Dodge <93716715+AnastasiiaKrtsk@users.noreply.github.com>
2024-01-10 08:10:12 +0200

    CounsumeTracker + welcome + layout

--- src/pages/WelcomePage/WelcomePage.styled.js
+++ src/pages/WelcomePage/WelcomePage.styled.js
@@ -1,7 +1,18 @@
 import styled from "styled-components";
-
 export const H1 = styled.h1`
-  font-weight: 700;
   font-size: 28px;
+  font-weight: var(--bold);
   line-height: 1.14;
+  margin-bottom: 16px;
+`;
+export const Descr = styled.h2`
+  font-size: 24px;
+  font-weight: var(--regular);
+  line-height: calc(30 / 24);
+  margin-bottom: 24px;
+
+  @media only screen and (min-width: 768px) {
+    font-size: 24px;
+    line-height: calc(32 / 26);
+  }
 `;
