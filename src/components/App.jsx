418eff3b3c225340d29aa2e6d13db7ecf2b8977b
domanskayelyzaveta <domanskaya_@ukr.net>
2024-01-10 18:44:43 +0200

    app fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from "./Dashboard/Dashboard";
 import WelcomePage from "../pages/WelcomePage/WelcomePage";
 import Layout from "./Layout/Layout";
-import UserSettings from "./Modals/UserSettings/UserSettings";
 // import PublicRoute from "./PublicRoute/PublicRoute";
 import SignInPage from "../pages/SignInPage/SignInPage";
 import SignUpPage from "../pages/SignUpPage/SignUpPage";
@@ -14,7 +13,6 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<WelcomePage />} />
           <Route path="/homepage" element={<Dashboard />} />
-          <UserSettings />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/signin" element={<SignInPage />} />
         </Route>
