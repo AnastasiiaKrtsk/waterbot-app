3c09dd745b1490aa3b68fe080782e4afbee631fe
Dodge <93716715+AnastasiiaKrtsk@users.noreply.github.com>
2024-01-10 10:31:06 +0200

    normalize+import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App"; // Import App without curly braces
 
-// import "./index.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
