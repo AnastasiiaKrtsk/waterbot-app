b6931f187eb72a744332105247608fafb7a22161
domanskayelyzaveta <domanskaya_@ukr.net>
2024-01-16 19:51:35 +0100

    to fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from 'path';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@mui/x-date-pickers': path.resolve(new URL('node_modules/@mui/x-date-pickers', import.meta.url).pathname),
+      "@mui/x-date-pickers": path.resolve(
+        new URL("node_modules/@mui/x-date-pickers", import.meta.url).pathname
+      ),
     },
   },
   plugins: [react()],
