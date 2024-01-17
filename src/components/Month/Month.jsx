816c7085cb313adb8728f9ead85784e698e67d57
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2024-01-17 15:25:32 +0100

    tooltips for days added

--- src/components/Month/Month.jsx
+++ src/components/Month/Month.jsx
@@ -1,4 +1,5 @@
 import moment from "moment";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
 import { useEffect, useState } from "react";
 import {
   StyledMonthChanger,
@@ -14,11 +15,32 @@ import {
 } from "./Month.styled";
 
 import sprite from "../../images/svg+logo/sprite.svg";
+import styled from "styled-components";
+
+const CustomWidthTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))({
+  [`& .${tooltipClasses.tooltip}`]: {
+    width: 292,
+    height: 188,
+    backgroundColor: "#fff",
+    borderRadius: "10px",
+    boxShadow: "0px 4px 4px 0px rgba(64, 123, 255, 0.30)",
+    display: "flex",
+    padding: "24px 16px",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    gap: "16px",
+    color:"var(--dark-blue)"
+  },
+});
 
 const Month = () => {
   const [shownDate, setShownDate] = useState(moment());
   const [daysInMonth, setDaysInMonth] = useState(moment().daysInMonth());
   const [isCurrentMonth, setIsCurrentMonth] = useState(false);
+
+
 
   useEffect(() => {
     setDaysInMonth(shownDate.daysInMonth());
@@ -38,6 +60,12 @@ const Month = () => {
     { length: daysInMonth },
     (_, index) => index + 1
   );
+
+  const longText = `
+Aliquam eget finibus ante, non facilisis lectus. Sed vitae dignissim est, vel aliquam tellus.
+Praesent non nunc mollis, fermentum neque at, semper arcu.
+Nullam eget est sed sem iaculis gravida eget vitae justo.
+`;
   return (
     <div>
       <StyledMonthWrapper>
@@ -50,19 +78,27 @@ const Month = () => {
           </StyledPrevMonth>
           <StyledMonthName>{shownDate.format("MMMM YYYY")}</StyledMonthName>
           <StyledNextMonth onClick={nextMonth} disabled={isCurrentMonth}>
-          <svg width={"16px"} height={"16px"}>
+            <svg width={"16px"} height={"16px"}>
               <use href={sprite + "#icon-right"}></use>
             </svg>
           </StyledNextMonth>
         </StyledMonthChanger>
       </StyledMonthWrapper>
       <StyledMonthWaterList>
-        {daysArray.map((day) => (
-          <StyledWaterListItemWrapper key={day}>
-            <StyledMonthWaterItem>{day}</StyledMonthWaterItem>
-            <StyledPercentage>0%</StyledPercentage>
-          </StyledWaterListItemWrapper>
-        ))}
+        {daysArray.map((day) => {
+          const placement = [1, 2, 3, 4, 11, 12, 13, 14, 21, 22, 23, 24, 31].includes(day)
+          ? "top-start"
+          : "top-end";
+          return (
+
+            <CustomWidthTooltip title={day} key={day} placement={placement}>
+            <StyledWaterListItemWrapper key={day}>
+              <StyledMonthWaterItem>{day}</StyledMonthWaterItem>
+              <StyledPercentage>0%</StyledPercentage>
+            </StyledWaterListItemWrapper>
+          </CustomWidthTooltip>
+            )
+          })}
       </StyledMonthWaterList>
     </div>
   );
