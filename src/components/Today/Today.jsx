6e0422e4a3ba5b3b21fa9cae66f7385f916a4c69
Gunnar97 <135347097+Gunnar97@users.noreply.github.com>
2024-01-18 14:42:59 +0200

    add map today

--- src/components/Today/Today.jsx
+++ src/components/Today/Today.jsx
@@ -16,6 +16,7 @@ import { useDispatch, useSelector } from "react-redux";
 import sprite from "../../images/svg+logo/sprite.svg";
 import { selectOpenModal, selectTodayWater } from "../../redux/selectors";
 import { setModalContent, setModalStatus } from "../../redux/slice";
+import moment from "moment";
 
 const Today = () => {
   const dispatch = useDispatch();
@@ -36,25 +37,35 @@ const Today = () => {
     dispatch(setModalContent("Add water"));
   };
 
+  const waterDayListItems = todayWater.userWaterDay?.map(
+    ({ _id, waterVolume, date }) => (
+      <StyledTodayWaterItem key={_id}>
+        <StyledWaterInfo>
+          <svg width={"36px"} height={"36px"}>
+            <use href={sprite + "#icon-water-glass"}></use>
+          </svg>
+          <StyledWaterVolume>{waterVolume} ml</StyledWaterVolume>{" "}
+          {moment(date).format("LT")}
+        </StyledWaterInfo>
+        <StyledTodayButtonsWrapper>
+          <EditWater onClick={handleEditWater} />
+          <DeleteWater onClick={handleDeleteWater} />
+        </StyledTodayButtonsWrapper>
+      </StyledTodayWaterItem>
+    )
+  );
+
+  //   _id(pin):"65a9194f061a6952026db88c"
+  // waterVolume(pin):123
+  // date(pin):"2024-01-18T01:02:00.000Z"
+  // owner(pin):"65a835fe8acf942ad1efbfd5"
+  // createdAt(pin):"2024-01-18T12:27:59.660Z"
+  // updatedAt(pin):"2024-01-18T12:27:59.660Z"
+
   return (
     <StyledTodayWrapper>
       <StyledTodayTitle>Today</StyledTodayTitle>
-
-      <StyledTodayWaterList>
-        <StyledTodayWaterItem>
-          <StyledWaterInfo>
-            <svg width={"36px"} height={"36px"}>
-              <use href={sprite + "#icon-water-glass"}></use>
-            </svg>
-            <StyledWaterVolume>250ml</StyledWaterVolume> Time
-          </StyledWaterInfo>
-          <StyledTodayButtonsWrapper>
-            <EditWater onClick={handleEditWater} />
-            <DeleteWater onClick={handleDeleteWater} />
-          </StyledTodayButtonsWrapper>
-        </StyledTodayWaterItem>
-      </StyledTodayWaterList>
-
+      <StyledTodayWaterList>{waterDayListItems}</StyledTodayWaterList>
       <AddWater onClick={handleAddWater} />
     </StyledTodayWrapper>
   );
