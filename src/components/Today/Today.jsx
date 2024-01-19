6c96e8ad4c8a40711b793799b77a54f731593f74
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2024-01-19 12:28:02 +0100

    dates fixes

--- src/components/Today/Today.jsx
+++ src/components/Today/Today.jsx
@@ -40,6 +40,8 @@ const Today = () => {
     dispatch(getWaterMonthThunk(monthYear));
   }, []);
 
+  
+
   const modalStatus = useSelector(selectOpenModal);
   const todayWater = useSelector(selectTodayWater);
 
@@ -67,7 +69,8 @@ const Today = () => {
             <use href={sprite + "#icon-water-glass"}></use>
           </svg>
           <StyledWaterVolume>{waterVolume} ml</StyledWaterVolume>{" "}
-          {moment(date).format("LT")}
+          {/* {new Date(date).getUTCHours()} */}
+          {moment(date).utc().format("LT")}
         </StyledWaterInfo>
         <StyledTodayButtonsWrapper>
           <EditWater onClick={() => handleEditWater(_id)} />
